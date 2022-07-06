fe5981f0328da86b1c60896cc8bcad3788cabf13
Glen <glen84_dr@hotmail.com>
2022-07-07 00:24:56 +0400

    Paddle class and two paddle objects

--- pong_game.py
+++ pong_game.py
@@ -1,10 +1,37 @@
 import pygame, sys
 
+class Ball:
+    def __init__(self, screen, color, x_coord, y_coord, radius):
+        self.screen = screen
+        self.color = color
+        self.x_coord = x_coord
+        self.y_coord = y_coord
+        self.radius = radius
+        self.show()
+
+    def show(self):
+        pygame.draw.circle(self.screen, self.color, (self.x_coord, self.y_coord), self.radius)
+
+
+class Paddle:
+    def __init__(self, screen, color, x_coord, y_coord, width, height):
+        self.screen = screen
+        self.color = color
+        self.x_coord = x_coord
+        self.y_coord = y_coord
+        self.width = width
+        self.height = height
+        self.show()
+
+    def show(self):
+        pygame.draw.rect(self.screen, self.color, (self.x_coord, self.y_coord, self.width, self.height))
+
+
+
 pygame.init()
 
-WIDTH = 800
-HEIGHT = 650
-
+WIDTH = 850
+HEIGHT = 600
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 screen.fill((127,255,0))
@@ -15,10 +42,19 @@ def line_and_background():
     pygame.draw.line(screen, (255,255,255),(WIDTH//2, 0),(WIDTH//2, HEIGHT), 5)
 
 line_and_background()
+
+ball = Ball(screen, (255, 0, 0), WIDTH//2, HEIGHT//2, 10)
+left_paddle = Paddle(screen, (20,144,255), 15, HEIGHT//2-55, 15, 110)
+right_paddle = Paddle(screen, (255,255,0), 820, HEIGHT//2-55, 15, 110) # OR WIDTH-15-15
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
         pygame.display.update()
+
+
+
 
